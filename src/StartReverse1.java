38921854bc0f1cbd56ab875c65cd751369d60732
1john0215 <jsu5620@naver.com>
2023-08-16 16:46:06 +0900

    dddddddddddddddd

--- src/StartReverse1.java
+++ src/StartReverse1.java
@@ -3,7 +3,7 @@ public class StartReverse1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-geasdfasdfnerated method stub
-           System.out.println("asdfasdf"); 
+           System.out.println("asdfasdfasdf"); 
 	}
 
 }
